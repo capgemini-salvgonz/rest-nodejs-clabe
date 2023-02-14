43cc77c11fb8768e6bc01ea49970da11a6a7181d
El Chava <salvador.gnolasco@gmail.com>
2023-02-13 19:44:03 -0600

    Base of server configuration

--- infraestructure/server.js
+++ infraestructure/server.js
@@ -24,4 +24,28 @@
 * Fecha de creación: Feb 13, 2023
 */
 
-require('dotenv').config();
+const express = require('express');
+
+
+class Server {
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.routes();
+  }
+
+  routes() {
+    this.app.get('/', (req, res) => {
+      res.send('Hello World')
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Servidor corriendo en el puerto', this.port);
+    });
+  }
+}
+
+module.exports = Server;
